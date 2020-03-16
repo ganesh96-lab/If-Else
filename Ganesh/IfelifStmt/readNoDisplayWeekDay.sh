51b1b3d40143a88d5fc3679d56658139b8c7c657
ganesh96-lab <54699093+ganesh96-lab@users.noreply.github.com>
2020-03-16 15:24:45 +0530

    Indentation

--- Ganesh/IfelifStmt/readNoDisplayWeekDay.sh
+++ Ganesh/IfelifStmt/readNoDisplayWeekDay.sh
@@ -8,26 +8,26 @@ then
 	echo"sunday"
 elif [ $number -eq 1 ]
 then
-   echo "monday"
+	echo "monday"
 elif [ $number -eq 2 ]
 then
-   echo "tuesday"
+	echo "tuesday"
 
 elif [ $number -eq 3 ]
-then 
-   echo "wednesday"
+then
+	echo "wednesday"
 
 elif [ $number -eq 4 ]
 then
-   echo "Thrusday"
+	echo "Thrusday"
 
 elif [ $number -eq 5 ]
 then
-   echo "Friday"
+	echo "Friday"
 
 elif [ $number -eq 6 ]
 then
-   echo "Saturday"
+	echo "Saturday"
 else
 	echo "invalid input"
 fi
